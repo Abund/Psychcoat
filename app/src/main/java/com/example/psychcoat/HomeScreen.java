05a42f303e58784bc916b42138da31d7c64ee283
Abund <abunonyeodi@gmail.com>
2020-07-03 15:07:10 +0100

    Implemented the menu of the application

--- app/src/main/java/com/example/psychcoat/HomeScreen.java
+++ app/src/main/java/com/example/psychcoat/HomeScreen.java
@@ -1,14 +1,129 @@
 package com.example.psychcoat;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class HomeScreen extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+import static com.google.firebase.storage.FirebaseStorage.getInstance;
+
+public class HomeScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+    private TextView clientName,profileName;
+    private ImageView imageViewProfile;
+    FirebaseAuth firebaseAuth;
+    boolean closeApp;
+    private int TAKE_IMAGE_CODE=10001;
+    private static  final  int PICK_IMAGE=1;
+    //storage
+    StorageReference storageReference;
+    //path where images of user profile and cover will be stored
+    String storagePath = "Users_Profile_Cover_Imgs/";
+    private  static final String TAG="HomeScreenActivity";
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+
+    String mUID;
+    FirebaseUser user;
+
+    ActionBar actionBar;
+
+
+    //progress dialog
+    ProgressDialog progressDialog;
+
+
+    //permissions constants
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final int STORAGE_REQUEST_CODE = 200;
+    private static final int IMAGE_PICK_GALLERY_CODE = 300;
+    private static final int IMAGE_PICK_CAMERA_CODE = 400;
+
+    //arrays of permissions to be requested
+    String cameraPermissions[];
+    String storagePermissions[];
+
+    //uri of picked image
+    Uri image_uri;
+
+    //for checking profile or cover photo
+    String profileOrCoverPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //init firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Psychologist");
+        storageReference = getInstance().getReference(); //firebase storage reference
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        //FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+
+            Fragment newFragment =  new HomeFragment();
+//            FragmentTransaction ft = getFragmentManager().beginTransaction();
+//            ft.add(R.id.content_frame, newFragment).commit();
+            replaceFragment(newFragment);
+        }  if (id == R.id.sign_out) {
+
+            firebaseAuth = FirebaseAuth.getInstance();
+            //LoginManager.getInstance().logOut();
+            firebaseAuth.signOut();
+            Intent at = new Intent(HomeScreen.this, MainActivity.class);
+            startActivity(at);
+        }
+//
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void replaceFragment(Fragment destFragment)
+    {
+        // First get FragmentManager object.
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.content_frame, destFragment);
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.addToBackStack(null).commit();
     }
 }
